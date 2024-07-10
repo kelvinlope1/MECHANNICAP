fe1a16b266e9a0137390620b4dc8ded3a2d2e2ed
kelvinlopez1 <kelvinlopez060301@gmail.com>
2024-07-10 19:11:37 -0400

    Subiendo los cambios para el sitio

---
+++ resources/views/home.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+     <link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+   
+
+</head>
+<body>
+    <header>
+        <h2 class = "logo">MechanicApp</h2>
+
+        <nav class ="menu">
+        <a href= "#">Inicio</a>
+        <a href= "#">Sobre Nosotros</a>
+        <a href= "#">Contactos</a>
+
+    </nav>
+    </header>
+    <section id = "banner">
+        <div class ="contenido">
+            <h3><span>MECHANICAPP: </span>Ven Manten tu auto <br>en los mejores Estados</h3>
+            <a hreaf= "#" class ="bEmpesar">Empezar</a>
+
+        </div>
+    </section>
+    <section id = "Informacion">
+        <div>
+        <h4>CAPACITACIÓN</h4>
+        <p>Nuestros asesores y técnicos automotrices <br>
+            están altamente capacitados</p>
+            <a href = "#" class = "SaberMas">Saber Mas</a>        
+        </div>
+        <div>
+        <h4>INTERNACIONAL</h4>
+         <p>Somos más de 1000 franquicias alrededor del mundo, mismas
+        <br> que confirman la entera satisfacción de nuestros clientes</p>
+        <a href = "#" class = "SaberMas">Saber Mas</a>   
+         </div>
+         <div>
+        <h4>TECNOLOGÍA</h4>
+        <p>Nuestra alta tecnología nos permite realizar 
+            el correcto diagnóstico a laprimera, <br>con garantía 
+            or escrito 6 meses o 10,000 km<br></p>
+            <a href = "#" class = "SaberMas">Saber Mas</a>
+        </div>
+
+    </section>
+
+
+
+</body>
+</html>
